fd45d1c15f1a3e34b1cc9816a7f10eff0bece9f6
Bhavyasri-Sikhakolli <bhavyasrisikhakolli127@gmail.com>
2026-08-27 20:35:12 +0530

    Update CGPA calculator

--- cgpa_calculator.py
+++ cgpa_calculator.py
@@ -1,15 +1,10 @@
-n = int(input("Enter number of subjects: "))
-
-total_points = 0
-total_credits = 0
-
-for i in range(n):
-    grade_point = float(input(f"Enter grade point for subject {i + 1}: "))
-    credits = float(input(f"Enter credits for subject {i + 1}: "))
-
-    total_points += grade_point * credits
-    total_credits += credits
-
-cgpa = total_points / total_credits
-
-print(f"Your CGPA is: {cgpa:.2f}")
+name=input("Enter Student name:")
+num_subjects=int(input("Enter number of subjects:"))
+total_grade_points=0
+for i in range(num_subjects):
+    grade_point=float(input(f"Enter grade point for subjects {i+1}:"))
+    total_grade_points+=grade_point
+cgpa=total_grade_points/num_subjects
+print("\n----Student Result----")
+print("Student name:",name)
+print("CGPA:",round(cgpa,2))
